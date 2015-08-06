f6e17f7f1f77717732af58644c4a002439490cef
steven harradine <steven.harradine@telus.com>
2015-08-05 22:34:36 -0400

    remove a site, add request dependancey, loop through sites and get status codes

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-var sites = require("./sites.json")
+var sites   = require("./sites.json"),
+    request = require('request')
 
 for (i in sites) {
-	console.log (sites[i].site)
+  testSiteAllProtocols (sites[i].site)
+}
+
+function testSiteAllProtocols (site) {
+  var protocals = [{
+    "protocal": "http",
+    "status": "",
+  },{
+    "protocal": "https",
+    "status": "",
+  }]
+
+  for (i in protocals) {
+    testSite (protocals[i].protocal + "://" + site)
+  }
+}
+
+function testSite (site) {
+  console.log ("Testing " + site)
+
+  options = {
+  	"url": site,
+  	"followRedirect": false
+  }
+
+  request(options, function (error, response, body) {
+  	if (!error) {
+  	  console.log (response.request.uri.protocol + response.statusCode)
+  	} else {
+  	  console.log ("Error: " + error)
+  	}
+  })
 }
